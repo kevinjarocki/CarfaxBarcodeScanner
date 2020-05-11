637688ecfb363c8b327e4cd7ef805ba1f2a37c8f
Kevin Jarocki <KJarocki@carfax.ca>
2020-05-11 14:44:03 -0400

    initial commit

---
+++ BarcodeScanner.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect, Component } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
